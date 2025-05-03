54b03d1a0de54fe984cbd5782c9d20b09c1f9a67
Avinash Singh <avi104@yahoo.co.in>
2025-05-03 18:14:09 +0530

    ceated secrets.toml

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ if not st.session_state.get("authenticated"):
     <div style="display: flex; flex-direction: column; align-items: flex-start; margin-left: 10px; margin-top: 10px;">
         <img src="data:image/png;base64,{logo_base64}" style="height: 60px; margin-bottom: 10px;" />
         <h1 style="margin: 0; font-size: 2.2rem; color: #010101;">Management Evasiveness Analyzer</h1>
-        <p style="margin: 4px 0 0 0; font-size: 1.1rem; color: #444;">AI-Powered Evasiveness Detection from Earnings Calls</p>
+        <p style="margin: 4px 0 0 0; font-size: 1.1rem; color: #444;">Evasiveness Detection from Earnings Calls</p>
     </div>
     """, unsafe_allow_html=True)
 
