62fb4cbdfca72f2cf1c921c46fd0a9dd7e543b96
Avinash Singh <avi104@yahoo.co.in>
2025-05-03 18:12:00 +0530

    ceated secrets.toml

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ if not st.session_state.get("authenticated"):
     st.markdown(f"""
     <div style="display: flex; flex-direction: column; align-items: flex-start; margin-left: 10px; margin-top: 10px;">
         <img src="data:image/png;base64,{logo_base64}" style="height: 60px; margin-bottom: 10px;" />
-        <h1 style="margin: 0; font-size: 2.2rem; color: #010101;">Management Tone Analyzer</h1>
+        <h1 style="margin: 0; font-size: 2.2rem; color: #010101;">Management Evasiveness Analyzer</h1>
         <p style="margin: 4px 0 0 0; font-size: 1.1rem; color: #444;">AI-Powered Evasiveness Detection from Earnings Calls</p>
     </div>
     """, unsafe_allow_html=True)
@@ -326,7 +326,7 @@ div[data-baseweb="input"] > div > div:last-child {{
 
 <div style="display: flex; flex-direction: column; align-items: center; margin-bottom: 20px;">
     <img src="data:image/png;base64,{logo_base64}" style="height: 40px; max-width: 240px; margin-bottom: 10px;" />
-    <h1 style="margin: 0; font-size: 2.4rem; color: #010101;">Management Tone Analyzer</h1>
+    <h1 style="margin: 0; font-size: 2.4rem; color: #010101;">Management Evasiveness Analyzer</h1>
     <p style="margin: 6px 0 0 0; font-size: 1.1rem; color: #010101;">AI-Powered Evasiveness Detection from Earnings Calls</p>
 </div>
 """, unsafe_allow_html=True)
